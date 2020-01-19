77b7c0b244cc7bebaeb36a49c7e810dd805d9c29
helguts <j.hecht88@web.de>
2020-01-19 18:47:09 +0100

    cleaned up unused functions

--- static/home.js
+++ static/home.js
@@ -28,45 +28,7 @@ function showGifsForText(searchText) {
      });
  };
 
-
-
-function showWord(wordType) {
-     $.post('/load_words', { word_type: wordType },
-        function(returnedWord) {
-        $('#theword').text(wordType + ": " + returnedWord);
-        showGif(returnedWord);
-    });
-}
-
-function showGif(word) {
-     $.post('/load_gif', { search_term: word },
-        function(returnedGifUrl) {
-            if (!returnedGifUrl) {
-                return false;
-             }
-
-            if (validURL(returnedGifUrl)) {
-                $('#thegif').attr("src", returnedGifUrl);
-            }
-            else {
-                alert(returnedGifUrl + " is not a valid url!");
-            }
-    });
-}
-
-function showSentence() {
-    $.post('/get_sentence',
-        function(data) {
-        $('#thesentence').text(data)
-    });
-}
-
 $(function() {
-  $('#btNouns').click(function(event) { showWord("noun"); });
-  $('#btAdj').click(function(event) { showWord("adj"); });
-  $('#btAdv').click(function(event) { showWord("adv"); });
-  $('#btVerbs').click(function(event) { showWord("verb"); });
-
   $('#btSentence').click(function(event) { showSentence(); });
   $('#btSearchGifForText').click(function(event) { showGifsForText($('#searchtext').val()); });
   return true;
